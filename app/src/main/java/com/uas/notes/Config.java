507448a94b4f294d10cf41b03c7932f394e47f13
HikalMuhamadAlfaridzhi <haikal.artha@gmail.com>
2023-08-10 15:58:42 +0700

    initial commit

---
+++ app/src/main/java/com/uas/notes/Config.java
@@ -0,0 +1,15 @@
+/**
+ * NIM : 10120023
+ * NAMA : Hikal Muhamad Alfaridzhi
+ * KELAS : IF-1
+ */
+package com.uas.notes;
+
+public class Config {
+
+    public static String DB_URL = "https://uas-10120023-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    public static String getDB_URL() {
+        return DB_URL;
+    }
+}
---
+++ app/src/main/java/com/uas/notes/model/NoteCategory.java
@@ -0,0 +1,10 @@
+/**
+ * NIM : 10120023
+ * NAMA : Hikal Muhamad Alfaridzhi
+ * KELAS : IF-1
+ */
+package com.uas.notes.model;
+
+public class NoteCategory {
+    public String title, total;
+}
